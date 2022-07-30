0edfcdfd8f4f2d02615d71f502d42de5dceb9690
品 <905991211@qq.com>
2022-07-30 14:10:15 +0800

    去掉elementui

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './route/index,js'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
 
 createApp(App)
 .use(router)
-.use(ElementPlus)
+// .use(ElementPlus)
 .mount('#app')
 
 
